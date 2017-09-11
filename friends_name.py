a111636ce856320f41f7613f143b138339b454dc
zhang jinpeng <jinpeng.zhang@outlook.com>
2017-09-11 20:50:06 +0800

    add friends_name.py

---
+++ friends_name.py
@@ -0,0 +1,8 @@
+names = ['zhang jin peng','wu man ni','xu xiao wei','yang min','zheng qing hua']
+num_list = [0,1,2,3,4]
+#print(names[0])
+#print(names[1])
+#print(names[2])
+#print(names[3])
+for i in num_list:
+    print("Hello! "+names[i]+",nice to meet you!")

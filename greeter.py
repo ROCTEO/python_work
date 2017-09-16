89ae24f7e2fc99f103e5628ce1f9d2e27c3371fe
zhang jinpeng <jinpeng.zhang@outlook.com>
2017-09-16 17:06:58 +0800

    add def display_massage()

--- greeter.py
+++ greeter.py
@@ -1,6 +1,12 @@
 #-*-coding:utf-8-*-
-def greet_user():
+def greet_user(user_name):
     """显示简单的问候语"""
-    print("Hello!")
+    print("Hello!"+user_name.title()+"!")
 
-greet_user()
+greet_user('zhang jin peng')
+
+def display_message():
+    """打印第八章的内容"""
+    print("1、本章学习了定义函数\n2、创建时函数名括号的变量为形参\n3、调用时括号内的变量为实参")
+
+display_message()

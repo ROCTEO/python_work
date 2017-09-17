89c81791936f526be9105b95ba8e36dd7de4c31a
zhang jinpeng <jinpeng.zhang@outlook.com>
2017-09-17 23:26:24 +0800

    编辑 greeter.py

--- greeter.py
+++ greeter.py
@@ -10,3 +10,9 @@ def display_message():
     print("1、本章学习了定义函数\n2、创建时函数名括号的变量为形参\n3、调用时括号内的变量为实参")
 
 display_message()
+
+def favorite_book(title):
+    """打印我最喜欢的书记"""
+    print("One of my favorite book is "+title.title()+".")
+
+favorite_book('alice in wonderland')
